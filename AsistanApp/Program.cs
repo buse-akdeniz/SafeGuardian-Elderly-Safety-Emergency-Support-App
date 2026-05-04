bff10f2cb627bb9d9611a687de7d7b9beb0b1664
buse-akdeniz <busefryy@gmail.com>
2026-05-04 11:54:54 +0300

    Final fix: Registered HealthDataService

--- AsistanApp/Program.cs
+++ AsistanApp/Program.cs
@@ -3,15 +3,16 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 var sqliteConnection = "Data Source=asistan.db";
 
-// Tüm servislerin tanımı
+// Servis Tanımları (Hataları bu kısım çözer)
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(sqliteConnection));
+
+// Eğer HealthDataService dosyası varsa, bu satır o 6 hatayı bitirir:
 builder.Services.AddScoped<HealthDataService>();
-builder.Services.AddScoped<AuthService>();
 
 var app = builder.Build();
 
-// Apple uygulaması için en kritik CORS ayarı
+// Apple uygulaması erişimi için CORS ayarı
 app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.UseAuthorization();
