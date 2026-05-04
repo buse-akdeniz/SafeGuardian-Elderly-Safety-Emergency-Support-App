b855933721c502e9935b988c08f1f9b6e17fc36b
buse-akdeniz <busefryy@gmail.com>
2026-05-04 11:46:08 +0300

    Fix: Added missing HealthDataService to Program.cs

--- AsistanApp/Program.cs
+++ AsistanApp/Program.cs
@@ -3,12 +3,16 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 var sqliteConnection = "Data Source=asistan.db";
 
+// Servisleri ekliyoruz (HealthDataService hatasını bu satır çözer)
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(sqliteConnection));
+builder.Services.AddScoped<HealthDataService>(); // Eksik olan parça buydu
 
 var app = builder.Build();
 
+// Apple ve tarayıcı erişimi için CORS
 app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
