6aa265e825693a57b70c9b8109b6ac121bb57142
buse-akdeniz <busefryy@gmail.com>
2026-05-05 12:28:33 +0300

    fix: change return types to dynamic to resolve property access errors

--- AsistanApp/Controllers/StateController.cs
+++ AsistanApp/Controllers/StateController.cs
@@ -1,4 +1,3 @@
-using AsistanApp.Services;using AsistanApp.Services;using System.Text.Json;
 using ilk_projem.Services;
 using ilk_projem.Models;
 
--- AsistanApp/Services/HealthDataService.cs
+++ AsistanApp/Services/HealthDataService.cs
@@ -6,12 +6,12 @@ namespace AsistanApp.Services
 {
     public class HealthDataService
     {
-        public async Task<dynamic> GetElderlySession(string token) => await Task.FromResult<dynamic>(new { Id = 1, Name = "Buse" });
+        public async Task<dynamic> GetElderlySession(string token) => await Task.FromResult<dynamic>(new { Id = 1, Name = "Buse", Email = "buse@test.com" });
         public async Task<List<dynamic>> GetFamilyMembers(int id) => await Task.FromResult(new List<dynamic>());
         public async Task<List<dynamic>> GetHealthRecords(int id, string type = null) => await Task.FromResult(new List<dynamic>());
-        public async Task<dynamic> AuthenticateElderly(string user, string pass) => await Task.FromResult<dynamic>(new { Token = "dummy", User = new { Id = 1 } });
+        public async Task<dynamic> AuthenticateElderly(string user, string pass) => await Task.FromResult<dynamic>(new { HasValue = true, Value = new { Token = "dummy", User = new { Id = 1 } } });
         public async Task AddHealthRecord(object record, string note = null, int? value = null) => await Task.CompletedTask;
-        public async Task<dynamic> GetUserState(int id) => await Task.FromResult<dynamic>(new { State = "Active" });
+        public async Task<dynamic> GetUserState(int id) => await Task.FromResult<dynamic>(new { Id = 1, CurrentContext = "Home", ActiveTaskId = 0, ScreenPriority = 1, IsAssistantActive = true, UpdatedAt = DateTime.Now });
         public async Task SetUserState(int id, object state) => await Task.CompletedTask;
     }
 
