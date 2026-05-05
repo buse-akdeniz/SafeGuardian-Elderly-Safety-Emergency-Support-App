bea160af59f986da12757cb87c9eb6d444919cf6
buse-akdeniz <busefryy@gmail.com>
2026-05-05 11:55:34 +0300

    fix: resolve dynamic lambda and void assignment errors

--- AsistanApp/Services/HealthDataService.cs
+++ AsistanApp/Services/HealthDataService.cs
@@ -1,15 +1,16 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AsistanApp.Services 
 {
     public class HealthDataService
     {
-        public dynamic GetElderlySession(string token) => new { Name = "Test", Email = "test@test.com", Id = 1 };
+        public object GetElderlySession(string token) => new { Name = "Test", Email = "test@test.com", Id = 1 };
         public List<object> GetFamilyMembers(int id) => new List<object>();
-        public List<dynamic> GetHealthRecords(int id, object type = null) => new List<object>();
-        public dynamic AuthenticateElderly(string user, string pass) => new { Token = "dummy-token", User = new { Id = 1, Name = "Buse" } };
+        public List<object> GetHealthRecords(int id, object type = null) => new List<object>();
+        public object AuthenticateElderly(string user, string pass) => new { Token = "dummy", User = new { Id = 1, Name = "Buse" } };
         public void AddHealthRecord(object record, string note = null, object value = null) { }
-        public dynamic GetUserState(int id) => new { State = "Stable" };
-        public void SetUserState(int id, string state) { }
+        public object GetUserState(int id) => new { State = "Stable" };
+        public object SetUserState(int id, object state) => new { Success = true };
     }
 }
